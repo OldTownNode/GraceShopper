6a36564ff950db3d587fa55c875daf2cfe7eee0d
Ashton <asch8216@gmail.com>
2019-11-07 09:40:06 -0500

    modularized single products and all products so components are reusable

---
+++ client/components/products-container.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import { fetchProducts } from '../store/product'
+import { connect } from 'react-redux'
+import SingleProduct from './single-product'
+
+class Products extends React.Component {
+	// constructor(props) {
+	// 	super(props)
+	// }
+
+	componentDidMount() {
+		this.props.fetchProducts()
+	}
+	render() {
+		return (
+			<div className="container">
+				{this.props.products.map(product => {
+					return <SingleProduct key={product.id} products={product} />
+				})}
+			</div>
+		)
+	}
+}
+
+const mapStateToProps = state => {
+	return {
+		products: state.productReducer.products
+	}
+}
+
+const mapDispatchToProps = dispatch => {
+	return {
+		fetchProducts: () => dispatch(fetchProducts())
+	}
+}
+
+const ConnectedProductsContainer = connect(mapStateToProps, mapDispatchToProps)(
+	Products
+)
+export default ConnectedProductsContainer
