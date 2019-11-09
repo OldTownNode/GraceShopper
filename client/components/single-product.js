f064449b4dfefb68250e6f9ec2bb88796e4b32cf
Ashton <asch8216@gmail.com>
2019-11-09 10:32:48 -0500

    style all products

--- client/components/single-product.js
+++ client/components/single-product.js
@@ -7,18 +7,20 @@ function SingleProduct(props) {
 	const { name, imageUrl, inventory, price, id } = props.products
 
 	return (
-		<div>
-			<Link to={`/products/${id}`}>
-				<h1>{name}</h1>
-			</Link>
+		<div className="card">
+			<div className="linkcontainer">
+				<Link to={`/products/${id}`}>
+					<h1 className="h1">{name}</h1>
+				</Link>
 
-			<a className="img">
-				<img src={imageUrl} />
-			</a>
-			<h2>
-				<div>{`price: ${price}`}</div>
-				<div>{`inventory: ${inventory}`}</div>
-			</h2>
+				<a className="imgcontainer">
+					<img className="img" src={imageUrl} />
+				</a>
+				<h2 className="priceinvcont">
+					<div>{`price: ${price}`}</div>
+					<div>{`inventory: ${inventory}`}</div>
+				</h2>
+			</div>
 		</div>
 	)
 }
