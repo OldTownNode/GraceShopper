2ed67fec05b3113ce4893f0b5449addabe9646f2
Ashton <asch8216@gmail.com>
2019-11-11 16:29:54 -0500

    update products works with bug in prepopulate

--- server/api/products.js
+++ server/api/products.js
@@ -20,4 +20,54 @@ router.get('/:id', async (req, res, next) => {
 	}
 })
 
+// eslint-disable-next-line complexity
+router.put('/:id', async (req, res, next) => {
+	try {
+		if (req.user.admin === true) {
+			let productObj = {}
+			const product = await Product.findByPk(req.params.id)
+			console.log('prod before update', product)
+			if (req.body.name !== '') {
+				productObj.name = req.body.name
+			} else {
+				productObj.name = product.name
+			}
+			if (req.body.description !== '') {
+				productObj.description = req.body.description
+			} else {
+				productObj.description = product.description
+			}
+
+			if (req.body.imageUrl !== '') {
+				productObj.imageUrl = req.body.imageUrl
+			} else {
+				productObj.imageUrl = product.imageUrl
+			}
+
+			if (req.body.inventory !== '') {
+				productObj.inventory = +req.body.inventory
+			} else {
+				productObj.inventory = +product.inventory
+			}
+			if (req.body.price !== '') {
+				productObj.price = +req.body.price
+			} else {
+				productObj.price = parseFloat(product.price)
+			}
+			if (req.body.category !== '') {
+				productObj.category = req.body.category
+			} else {
+				productObj.category = product.category
+			}
+			await product.update(productObj)
+			await product.save()
+			res.json(product)
+		} else {
+			res.send('only admins are able to update products')
+		}
+	} catch (error) {
+		next(error)
+	}
+})
+
 module.exports = router
