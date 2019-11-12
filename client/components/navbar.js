1126315cb271cc1bfc064ec01924f426a9ed565f
Ashton <asch8216@gmail.com>
2019-11-12 17:18:52 -0500

    added styling and features to navbar

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -4,48 +4,69 @@ import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
 import { logout } from '../store'
 
-const Navbar = ({ handleClick, isLoggedIn, user }) => (
-	<div className="nav-header">
-		<h1>OLD TOWN NODE</h1>
-		<nav>
-			{isLoggedIn ? (
-				<div>
-					{/* The navbar will show these links after you fa-9x log in */}
+const calculateItemsInCart = cart => {
+	const items = Object.values(cart)
+	console.log('items', items)
+	if (items.length == 0) {
+		return 0
+	} else {
+		return items.reduce((a, b) => a + b)
+	}
+}
 
-					<Link to="/home">Home</Link>
+const Navbar = ({ handleClick, isLoggedIn, user, cart }) => {
+	console.log('isloggedin', cart)
+	return (
+		<div className="nav-header">
+			<Link to="/">
+				<h1>OLD TOWN NODE</h1>
+			</Link>
 
-					<a href="#" onClick={handleClick}>
-						Logout
-					</a>
+			<nav>
+				{isLoggedIn ? (
+					<div>
+						{/* The navbar will show these links after you fa-9x log in */}
 
-					<Link to={`/users/${user.id}`}>Profile</Link>
-					<Link to="/products">Products</Link>
-					<Link to={`/users/${user.id}/cart`}>
-						<i className="fas fa-shopping-cart fa-2x" />
-					</Link>
-				</div>
-			) : (
-				<div>
-					{/* The navbar will show these links before you log in */}
-					<Link to="/login">Login</Link>
-					<Link to="/signup">Sign Up</Link>
-					<Link to="/products">Products</Link>
-					<Link to="/users/0/cart">
-						<i className="fas fa-shopping-cart fa-2x" />
-					</Link>
-				</div>
-			)}
-		</nav>
-		<hr />
-	</div>
-)
+						<a href="#" onClick={handleClick}>
+							Logout
+						</a>
+
+						<Link to={`/users/${user.id}`}>Profile</Link>
+						<Link to="/products">Products</Link>
+						<Link to={`/users/${user.id}/cart`}>
+							<i className="fas fa-shopping-cart fa-2x">
+								{calculateItemsInCart(cart)}
+							</i>
+						</Link>
+					</div>
+				) : (
+					<div>
+						{/* The navbar will show these links before you log in */}
+						<Link to="/login">Login</Link>
+						<Link to="/signup">Sign Up</Link>
+						<Link to="/products">Products</Link>
+						<Link to="/users/0/cart">
+							<i className="fas fa-shopping-cart fa-2x">
+								{calculateItemsInCart(cart)}
+							</i>
+						</Link>
+					</div>
+				)}
+			</nav>
+			<hr />
+		</div>
+	)
+}
 
 /**
  * CONTAINER
  */
 const mapState = state => {
+	console.log('state', state)
 	return {
-		isLoggedIn: !!state.user.loggedInUser.id || !!state.user.user,
+		cart: state.cart,
+		isLoggedIn: !!state.user.loggedInUser.id,
+		// isLoggedIn: !!state.user.loggedInUser.id || !!state.user.user,
 		user: state.user.loggedInUser
 	}
 }
