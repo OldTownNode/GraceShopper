a41f72a3278912cb774b15f695fd614c32c397bb
Dan Erbland <danerbland@gmail.com>
2019-11-13 10:47:11 -0500

    fixed stripe total bug

--- client/components/cart-view.js
+++ client/components/cart-view.js
@@ -32,7 +32,7 @@ class CartView extends React.Component {
 		let totalPrice = 0
 		let userparam = 0
 		if (this.props.userId) userparam = this.props.userId
-
+		console.log('type of total price: ', typeof totalPrice)
 		return (
 			<div>
 				{!userparam && !this.state.formCompleted ? (
@@ -96,7 +96,7 @@ class CartView extends React.Component {
 								Checkout
 							</button>
 						</div>
-						<CheckoutForm sum={(totalPrice / 100).toFixed(2)} />
+						<CheckoutForm sum={totalPrice} />
 					</div>
 				)}
 			</div>
